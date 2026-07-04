8eca1716882bbda85fb0c3cad6f7a7ddda19bbbe
homarugvedha-chappidi <homarugvedha@gmail.com>
2026-07-04 20:35:51 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,1 +1,21 @@
-{}
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home Page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# About Page
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# Find Your Crop Page
+@app.route("/findyourcrop")
+def findyourcrop():
+    return render_template("findyourcrop.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
